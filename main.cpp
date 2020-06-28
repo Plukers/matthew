21b62a4896029f3f3d52314e2c5ae42d5934d4bf
Lukas Prost <prost@posteo.de>
2020-06-28 16:53:51 +0200

    Crating window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <QCoreApplication>
+#include <QtWidgets/QWidget>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QWidget window;
+    window.resize(320, 240);
+    window.show();
+    window.setWindowTitle(
+      QCoreApplication::translate("title", "Matthew - Material Viewer"));
 
     return a.exec();
 }
